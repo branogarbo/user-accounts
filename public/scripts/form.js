0348604859f7af9eb3967b90532c103a384e12e2
Brian Longmore <brianl.ext@gmail.com>
2020-04-22 20:19:29 -1000

    disable form on submit to prevent excess post requests

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,31 +1,12 @@
-// figure out the ajax
-
-//async function submit() {
-//   let options = {
-//      method: 'POST',
-//      headers: {
-//         'Content-Type': 'application/json',
-//         body: JSON.stringify({
-//            username: document.getElementsByName('username').value,
-//            password: document.getElementsByName('password').value
-//         })
-//      }
-//   }
-//   
-//   let res = await fetch(postRoute,options);
-//   res = await res.json();
-//
-//   document.querySelector('#authmsg').innerHTML = res.authmsg;   
-//}
-
 let el = selector => document.querySelectorAll(selector);
 
 el('form')[0].onsubmit = () => {
    el('#loader')[0].style.display = "flex";
+   el('form > input[type="submit"]')[0].setAttribute('disabled',true);
 };
 
 el('form > input').forEach(input => {
    input.onfocus = () => {
       el('#authmsg')[0].style.display = "none";
-   }
-})
+   };
+});
